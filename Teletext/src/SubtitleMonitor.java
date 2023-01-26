cf06e2ba12f5944637037f307464a2bfcc6e87da
Jari Saari <jari.m.saari@gmail.com>
2023-01-26 14:26:47 +0200

    Clean Subtitlemonitor

--- Teletext/src/SubtitleMonitor.java
+++ Teletext/src/SubtitleMonitor.java
@@ -3,6 +3,8 @@ import java.time.format.DateTimeFormatter;
 
 public class SubtitleMonitor {
 
+	/** Constants for coloring text. */
+	
 	public static final String ANSI_RESET = "\u001B[0m";
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -20,36 +22,42 @@ public class SubtitleMonitor {
 	public static final String ANSI_LBLUE = "\u001B[94m";
 	public static final String ANSI_LMAGENTA = "\u001B[95m";
 	public static final String ANSI_LCYAN = "\u001B[97m";
-
+	
 	public static final DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("HH:mm");
 	
 	public static void main(String[] args) {
 		
+		/** Filter for current programs. */
 		ChangeMonitor.followedPackets[320] = 0b01000001000001000000000000000000;		
 		
+		/** Filter for subtitle locations. */
 		int subtextFilter = (0x1 << (32-20) | (0x1 << (32-22)));
 		
+		// TV1, TV2 and Teema & Fem in finnish
 		ChangeMonitor.followedPackets[333] = subtextFilter;
 		ChangeMonitor.followedPackets[334] = subtextFilter;
 		ChangeMonitor.followedPackets[336] = subtextFilter;
 		
 		/*
+		// TV1, TV2 and Teema & Fem in swedish
 		followedPackets[771] = subtextFilter;
 		followedPackets[772] = subtextFilter;
 		followedPackets[773] = subtextFilter;
 		*/
 		
+		// TV1, TV2 and Teema & Fem in finnish
 		ChangeMonitor.followedPackets[451] = subtextFilter;
 		ChangeMonitor.followedPackets[452] = subtextFilter;
 		ChangeMonitor.followedPackets[453] = subtextFilter;
 		
 		/*
+		// TV1, TV2 and Teema & Fem in swedish
 		followedPackets[455] = subtextFilter;
 		followedPackets[456] = subtextFilter;
 		followedPackets[457] = subtextFilter;
 		 */	
 
-		// Check only followed packets
+		/** Checks only followed packets */
 		while (true) {
 			
 			LineChange lc = ChangeMonitor.readChange();
@@ -58,29 +66,27 @@ public class SubtitleMonitor {
 				return;
 			}
 
-			String kanavas = "";
+			String channels = "";
 
 			if(lc.page == 320) {
-				kanavas = ANSI_LYELLOW+"***";
+				channels = ANSI_LYELLOW + "***";
 			}
 			if(lc.page == 333 || lc.page == 771 || lc.page == 451 || lc.page == 455) {
-				kanavas = ANSI_LBLUE + "<TV1>";
+				channels = ANSI_LBLUE + "<TV1>";
 			}
 			if(lc.page == 334 || lc.page == 772 || lc.page == 452 || lc.page == 456) {
-				kanavas = ANSI_LMAGENTA + "<TV2>";
+				channels = ANSI_LMAGENTA + "<TV2>";
 			}
 			if(lc.page == 336 || lc.page == 773 || lc.page == 453 || lc.page == 457) {
-				kanavas = ANSI_WHITE + "<Teema & Fem>";
+				channels = ANSI_WHITE + "<Teema & Fem>";
 			}
 		
 			LocalDateTime date = LocalDateTime.now();
 			
-			System.out.print(ANSI_WHITE + date.format(dateFormat) + " ");
-			
-			System.out.print(kanavas);
-			
-			System.out.println(lc.newline);
+			System.out.println(ANSI_WHITE + date.format(dateFormat) + " " + channels + lc.content);
 							
-		}		
+		}
+		
 	}
+	
 }
