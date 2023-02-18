644296985a280f25f5cdf1b7d43686a3b2eac278
Jari Saari <jari.m.saari@gmail.com>
2023-02-18 13:32:09 +0200

    Create own java-file for Data-class.

--- Teletext/src/EPGMonitor.java
+++ Teletext/src/EPGMonitor.java
@@ -27,47 +27,47 @@ public class EPGMonitor {
 		if(tag == 0x54) {
 
 			System.out.print(DESCIDENT + DvbReader.byteBuffertoHex(data) + "  ");
-			System.out.print(EPGReader.Data.nibbles[(EPGReader.Data.getContentNibble(data) & 0xF0) >> 4]);
+			System.out.print(EPGData.nibbles[(EPGData.getContentNibble(data) & 0xF0) >> 4]);
 			System.out.println();
 
 		}else if(tag == 0x55) {
 			
 			System.out.println(DESCIDENT + DvbReader.byteBuffertoHex(data));
 			
-			System.out.println(DESCIDENT + "Countries: " + EPGReader.Data.getParentalRatingDescriptorCountries());
+			System.out.println(DESCIDENT + "Countries: " + EPGData.getParentalRatingDescriptorCountries());
 			
-			for(int i=0;i<EPGReader.Data.getParentalRatingDescriptorCountries();i++){					
-				System.out.println(DESCIDENT + "  Country: " + EPGReader.Data.getParentalRatingDescriptorLang(i));
-				System.out.println(DESCIDENT + "  Age: " + EPGReader.Data.getParentalRatingDescriptorAge(i));				
+			for(int i=0;i<EPGData.getParentalRatingDescriptorCountries();i++){					
+				System.out.println(DESCIDENT + "  Country: " + EPGData.getParentalRatingDescriptorLang(i));
+				System.out.println(DESCIDENT + "  Age: " + EPGData.getParentalRatingDescriptorAge(i));				
 			}
 			
-			System.out.println(DESCIDENT + "  Age(FIN): " + EPGReader.Data.getParentalRatingDescriptorByBountry(EPGReader.Data.COUNTRY_FIN));
+			System.out.println(DESCIDENT + "  Age(FIN): " + EPGData.getParentalRatingDescriptorByBountry(EPGData.COUNTRY_FIN));
 			
 
 		}else if(tag == 0x4d) {
 
 			//if(EPGReader.Data.getShortEventDescriptorLangCode()==EPGReader.Data.COUNTRY_fin) {
-				if(EPGReader.Data.getShortEventDescriptorLang()!=null) {
-					System.out.println(DESCIDENT + "Lang: " + EPGReader.Data.getShortEventDescriptorLang());
+				if(EPGData.getShortEventDescriptorLang()!=null) {
+					System.out.println(DESCIDENT + "Lang: " + EPGData.getShortEventDescriptorLang());
 				}
 	
-				if(EPGReader.Data.getShortEventDescriptorName()!=null) {
-					System.out.println(DESCIDENT + "EventName: " + EPGReader.Data.getShortEventDescriptorName());
+				if(EPGData.getShortEventDescriptorName()!=null) {
+					System.out.println(DESCIDENT + "EventName: " + EPGData.getShortEventDescriptorName());
 				}
 	
-				if(EPGReader.Data.getShortEventDescriptorText()!=null) {
-					System.out.println(DESCIDENT + "Text: " + EPGReader.Data.getShortEventDescriptorText());
+				if(EPGData.getShortEventDescriptorText()!=null) {
+					System.out.println(DESCIDENT + "Text: " + EPGData.getShortEventDescriptorText());
 				}			  
 			//}
 
 		}else if(tag == 0x4e) {
 
-			String asString = EPGReader.Data.getDataAsText(data);
+			String asString = EPGData.getDataAsText(data);
 			System.out.println(DESCIDENT + asString);
 
 		}else {
 
-			System.out.println(DESCIDENT + EPGReader.Data.getDataAsText(data));
+			System.out.println(DESCIDENT + EPGData.getDataAsText(data));
 
 		}
 	}
