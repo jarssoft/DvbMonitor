10ae4dd54ec515e65e9974cb87893ad5e4bd5c2c
Jari Saari <jari.m.saari@gmail.com>
2023-02-19 12:30:24 +0200

    Use switch-case in Descriptor events.

--- Teletext/src/EPGMonitor.java
+++ Teletext/src/EPGMonitor.java
@@ -25,28 +25,34 @@ public class EPGMonitor {
 
 		System.out.println("    Desc: (e"+EPGEvent.getDescriptorLoopLenght() + ") " + DvbReader.byteBuffertoHex(DescriptorTL.buffer) + "  ");
 
-		if(tag == ContentDescriptor.TAG) {
-
-			System.out.print(DESCIDENT + DvbReader.byteBuffertoHex(Descriptor.buffer) + "  ");
-			System.out.print(ContentDescriptor.nibbles[(ContentDescriptor.getContentNibble(Descriptor.buffer) & 0xF0) >> 4]);
-			System.out.println();
-
-		}else if(tag == ParentalRatingDescriptor.TAG) {
-			
-			System.out.println(DESCIDENT + DvbReader.byteBuffertoHex(Descriptor.buffer));
-			
-			System.out.println(DESCIDENT + "Countries: " + ParentalRatingDescriptor.getCountries());
-			
-			for(int i=0;i<ParentalRatingDescriptor.getCountries();i++){					
-				System.out.println(DESCIDENT + "  Country: " + ParentalRatingDescriptor.getLang(i));
-				System.out.println(DESCIDENT + "  Age: " + ParentalRatingDescriptor.getAge(i));				
-			}
-			
-			System.out.println(DESCIDENT + "  Age(FIN): " + ParentalRatingDescriptor.getByBountry(Descriptor.COUNTRY_FIN));
-			
-		}else if(tag == ShortEventDescriptor.TAG) { 
-
-			//if(ShortEventDescriptor.getLangCode()==EPGData.COUNTRY_fin) {
+		switch (tag) {
+		
+		  case ContentDescriptor.TAG:
+			  
+			  System.out.print(DESCIDENT + DvbReader.byteBuffertoHex(Descriptor.buffer) + "  ");
+				System.out.print(ContentDescriptor.nibbles[(ContentDescriptor.getContentNibble(Descriptor.buffer) & 0xF0) >> 4]);
+				System.out.println();
+				
+		    break;
+		    
+		  case ParentalRatingDescriptor.TAG:
+			  
+				System.out.println(DESCIDENT + DvbReader.byteBuffertoHex(Descriptor.buffer));
+				
+				System.out.println(DESCIDENT + "Countries: " + ParentalRatingDescriptor.getCountries());
+				
+				for(int i=0;i<ParentalRatingDescriptor.getCountries();i++){					
+					System.out.println(DESCIDENT + "  Country: " + ParentalRatingDescriptor.getLang(i));
+					System.out.println(DESCIDENT + "  Age: " + ParentalRatingDescriptor.getAge(i));				
+				}
+				
+				System.out.println(DESCIDENT + "  Age(FIN): " + ParentalRatingDescriptor.getByBountry(Descriptor.COUNTRY_FIN));
+				
+		    break;
+		    
+		  case ShortEventDescriptor.TAG:
+		    
+				//if(ShortEventDescriptor.getLangCode()==EPGData.COUNTRY_fin) {
 				if(ShortEventDescriptor.getLang()!=null) {
 					System.out.println(DESCIDENT 
 							+ "Lang: " + ShortEventDescriptor.getLang());
@@ -62,12 +68,15 @@ public class EPGMonitor {
 							+ "Text: " + ShortEventDescriptor.getText());
 				}			  
 			//}
+				
+		    break;
 
-		}else {
-
-			System.out.println(DESCIDENT + Descriptor.getDataAsText(Descriptor.buffer));
-
+		  default:
+			  System.out.println(DESCIDENT + Descriptor.getDataAsText(Descriptor.buffer));
+			  
+		    break;
 		}
+
 	}
 
 	public static void main(String[] args) {
